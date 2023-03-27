4ecfcd02145222bbd338c13db26ed4bd10b6f47e
Bakabruh <chewie.louveau@gmail.com>
2023-03-27 22:12:42 +0200

    ADD search bar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -16,15 +16,42 @@ class _HomePageState extends State<HomePage> {
         child: Container(
           alignment: Alignment.topLeft,
           child: Column(
-            children: const [
-              Padding(
-                padding: EdgeInsets.only(right: 150), //apply padding to some sides only
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(right: 160), //apply padding to some sides only
                 child: Text("Demain,", style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
               ),
-              Padding(
-                padding: EdgeInsets.only(left: 20), //apply padding to some sides only
+              const Padding(
+                padding: EdgeInsets.only(left: 10), //apply padding to some sides only
                 child: Text("on mange sain 🥦", style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
               ),
+              Row(
+                children: const [
+                  Padding(
+                    padding: EdgeInsets.only(left: 25, bottom: 25), //apply padding to some sides only
+                    child: Icon(Icons.location_pin, color: Colors.grey),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 20),
+                    child: Text("IIM - 47 boulevard de Pesaro, Nanterre", style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold, color: Colors.grey)),
+                  )
+                ],
+              ),
+              const SizedBox(
+                width: 300,
+                child: TextField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search, color: Colors.black),
+                  hintStyle: TextStyle(color: Colors.black, fontSize: 13),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(width: 3, color: Colors.black),
+                    borderRadius: BorderRadius.zero
+                  ),
+                  hintText: 'Recherche un plat, un ingrédient...',
+                  contentPadding: EdgeInsets.symmetric(vertical: 10)
+                ),
+              ),
+              )
             ],
           ),
         ),
