9f3982cb4877720e03b1c0c3abf9567a211ab018
Bakabruh <chewie.louveau@gmail.com>
2023-03-28 23:16:01 +0200

    ADD tabs

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -13,22 +13,20 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          alignment: Alignment.topLeft,
           child: Column(
             children: [
               const Padding(
-                padding: EdgeInsets.only(right: 160), //apply padding to some sides only
+                padding: EdgeInsets.only(right: 180), //apply padding to some sides only
                 child: Text("Demain,", style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
               ),
               const Padding(
-                padding: EdgeInsets.only(left: 10), //apply padding to some sides only
+                padding: EdgeInsets.only(right: 10), //apply padding to some sides only
                 child: Text("on mange sain 🥦", style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
               ),
               Row(
                 children: const [
                   Padding(
-                    padding: EdgeInsets.only(left: 25, bottom: 25), //apply padding to some sides only
+                    padding: EdgeInsets.only(left: 15, bottom: 25), //apply padding to some sides only
                     child: Icon(Icons.location_pin, color: Colors.grey),
                   ),
                   Padding(
@@ -38,7 +36,7 @@ class _HomePageState extends State<HomePage> {
                 ],
               ),
               const SizedBox(
-                width: 300,
+                width: 320,
                 child: TextField(
                 decoration: InputDecoration(
                   prefixIcon: Icon(Icons.search, color: Colors.black),
@@ -51,10 +49,50 @@ class _HomePageState extends State<HomePage> {
                   contentPadding: EdgeInsets.symmetric(vertical: 10)
                 ),
               ),
+              ),
+              DefaultTabController(
+                length: 3,
+                initialIndex: 0,
+                child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: <Widget>[
+                  Container(
+                    child: const TabBar(
+                      labelColor: Colors.black,
+                      unselectedLabelColor: Colors.grey,
+                      indicatorColor: Colors.black,
+                      tabs: [
+                        Tab(text: 'Demain'),
+                        Tab(text: 'Après-demain'),
+                        Tab(text: 'Plus tard')
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 400,
+                    decoration: const BoxDecoration(
+                    border: Border(top: BorderSide(color: Colors.grey, width: 0.5))
+                    ),
+                    child: TabBarView(children: <Widget>[
+                  Container(
+                    child: const Center(
+                      child: Text('Demain', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                    ),
+                  ),
+                  Container(
+                    child: const Center(
+                      child: Text('Après-demain', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                    ),
+                  ),
+                  Container(
+                    child: const Center(
+                      child: Text('Plus tard', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                    ),
+                  ),
+                ]),
+                ),
+                ])
               )
             ],
           ),
-        ),
       ),
     );
   }
